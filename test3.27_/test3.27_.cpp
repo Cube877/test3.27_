0a30c76ca569e4f42fac1a699252868b7bca5bd6
Cube <877211593@qq.com>
2020-03-27 15:22:29 +0800

    wu

--- test3.27_/test3.27_.cpp
+++ test3.27_/test3.27_.cpp
@@ -1,20 +1,48 @@
 ﻿// test3.27_.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
-#include <iostream>
+#include<iostream>
+#include<opencv.hpp>
+
+using namespace cv;
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	VideoCapture cap;
+
+
+
+	cap.open(0);
+
+	while (1)
+	{
+		Mat frame;
+		Mat dst_mB;
+		Mat dst_blur;
+		Mat dst_GB;
+		Mat dst_sobelx;
+		Mat dst_sobely;
+		cap >> frame;
+		bool rSucess = cap.read(frame);
+		medianBlur(frame, dst_mB, 3);
+		blur(frame, dst_blur, Size(3, 3));
+		GaussianBlur(frame, dst_GB, Size(3, 3), 1, 0);
+		Sobel(frame, dst_sobelx, CV_16SC1, 1, 0, 3);
+		convertScaleAbs(dst_sobelx, dst_sobelx);
+		Sobel(frame, dst_sobely, CV_16SC1, 0, 1, 3);
+		convertScaleAbs(dst_sobely, dst_sobely);
+
+		imshow("frame", frame);            //原图
+		namedWindow("dst_mB", 0);
+		imshow("dst_mB", dst_mB);			//中值滤波
+		imshow("dst_blur", dst_blur);		//均值滤波
+		imshow("dst_GB", dst_GB);		//均值滤波-高斯
+		imshow("dst_sobelx", dst_sobelx);		//边缘提取-x
+		imshow("dst_sobely", dst_sobely);		//边缘提取-y
+
+		waitKey(30);
+	}
+	return 0;
 }
 
-// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
-// 调试程序: F5 或调试 >“开始调试”菜单
-
-// 入门使用技巧: 
-//   1. 使用解决方案资源管理器窗口添加/管理文件
-//   2. 使用团队资源管理器窗口连接到源代码管理
-//   3. 使用输出窗口查看生成输出和其他消息
-//   4. 使用错误列表窗口查看错误
-//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
-//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
